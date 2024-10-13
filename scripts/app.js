fe9c8559636f804dc60c05bbfcf45f25a1a6414f
xyryc <amdanikzz@gmail.com>
2024-10-13 23:47:12 +0600

    added static modal data fetch using id

--- scripts/app.js
+++ scripts/app.js
@@ -21,37 +21,47 @@ const displayAllMeals = (meals) => {
 
     div.innerHTML = `
         <div
-            class="flex items-center justify-center border rounded-lg hover:bg-gray-100 duration-200 ease-in-out"
+            class="flex flex-col lg:flex-row items-center justify-center border rounded-lg hover:bg-gray-100 duration-200 ease-in-out"
           >
             <!-- image -->
             <img
-              class="rounded-lg h-[300px] w-[230px] object-cover"
+              class="rounded-lg h-[300px] lg:w-[230px] w-full object-cover"
               src="${meal.strMealThumb}"
               alt=""
             />
             <!-- text -->
-            <div class="px-6">
+            <div class="p-6">
               <h3 class="text-2xl font-bold">${meal.strMeal}</h3>
               <p class="text-lg leading-[30px] text-[#706F6F] mt-4 mb-6">
                 This dish features a harmonious blend of flavors, with various interpretations available, 
                 ensuring a delightful culinary experience.
 
               </p>
-              <button class="text-[#FFC107] font-semibold text-lg underline">
-                View Details
+              <button
+                onclick="mealDetails('${meal.idMeal}')"
+                class="text-[#FFC107] font-semibold text-lg underline">
+                    View Details
               </button>
             </div>
         </div>
         `;
 
     mealsContainer.appendChild(div);
-
-    console.log(meal);
   });
 };
 
 const handleShowAll = () => {
   loadAllMeals(true);
+};
+
+const mealDetails = async (idMeal) => {
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+  );
+  const data = await response.json();
+  console.log(data.meals[0]);
+
+  detailsModal.showModal();
 };
 
 loadAllMeals();
